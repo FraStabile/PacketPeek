8ff892a6f6842a6edd1387087d7e8e691579a8f6
Francesco Stabile <stabile.francesco99@gmail.com>
2025-03-27 19:12:21 +0100

    Initial Commit

---
+++ ProxyApp/ProxyAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  ProxyAppApp.swift
+//  ProxyApp
+//
+//  Created by Francesco Stabile on 27/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ProxyAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ProxyAppTests/ProxyAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProxyAppTests.swift
+//  ProxyAppTests
+//
+//  Created by Francesco Stabile on 27/03/25.
+//
+
+import Testing
+@testable import ProxyApp
+
+struct ProxyAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
